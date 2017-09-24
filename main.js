14694178d80ea65c21546e51a84e11d36bd2b4f2
Adler Coelho Santos <adler.coelho.santos@icloud.com>
2017-09-24 18:43:10 -0300

    Organizando script de main.js.

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const electron = require('electron');
-const { app, BrowserWindow } = electron;
-const args = process.argv.slice(1);
+const electron = require('electron')
+,   { app, BrowserWindow } = electron
+,   args = process.argv.slice(1);
 
-let win, develop;
+let win, develop, platform = process.platform;
 
 develop = args.some(val => val === "--develop");
 
-app.on('ready', () => {
-    createMainElectronWindow();
-});
+console.log(`[Platform] 🖥  minha plataforma:  ${platform}.`); // darwin, win e lin
+
+console.log(`[Version] ⚙️  Versão do Electron: ${process.versions.electron}.`);
+console.log(`[Version] ⚙️  Versão do NodeJS: ${process.versions.node}.`);
+console.log(`[Version] ⚙️  Versão do Chromium: ${process.versions.chrome}.`);
 
 // Criando janela principal do App
-let createMainElectronWindow = () => {
-    let electronScreen = electron.screen;
-    let size = electronScreen.getPrimaryDisplay().workAreaSize;
+function createMainElectronWindow() {
+    let electronScreen = electron.screen
+    ,   size = electronScreen.getPrimaryDisplay().workAreaSize;
 
     win = new BrowserWindow({
         x: 0,
@@ -21,38 +23,23 @@ let createMainElectronWindow = () => {
         width: size.width,
         height: size.height,
         title: "Aplicação Angular dentro do Electron",
-        fullscreen: true
+        fullscreen: true,
+        show: false
     });
 
-    let url = `file://${__dirname}/index.html`;
-    win.loadURL(url);
-
-    if (develop) {
-        win.openDevTools();
-    }
+    win.loadURL(`file://${__dirname}/index.html`);
 }
 
-// Quit when all windows are closed.
+app.on('ready', createMainElectronWindow);
+
 app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    if (platform !== 'darwin') {
         app.quit();
     }
 });
 
-// Emitted when the window is closed.
-app.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-});
-
-app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
+app.on('activate', _ => {
+    if (win === null || platform === 'darwin') {
         createMainElectronWindow();
     }
 });
